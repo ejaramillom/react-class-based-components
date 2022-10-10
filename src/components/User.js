32f51f21954351a8f6d66be31ff255765c4a2d0a
Emmanuel <ejaramilo@hotmail.com>
2022-10-10 15:27:42 -0500

    165 adding a first class based component

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,22 @@
-import classes from './User.module.css';
+import { Component } from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from "./User.module.css";
+
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// i extended the component so i can use this.props
+
+// in class components render does not receive props
+// wee need to import component
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+// so we just converted the functional component to a class based component
 
 export default User;
